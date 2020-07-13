613b80327f22a663d5e996faab700e099c805d88
tarricsookdeo <tarricsookdeo@outlook.com>
2020-07-13 05:25:29 -0400

    Added comment

--- config/db.js
+++ config/db.js
@@ -8,7 +8,7 @@ const connectDB = async () => {
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
-    process.exit(1);
+    process.exit(1); // Exits process with failure
   }
 };
 
